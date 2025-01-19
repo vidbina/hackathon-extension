f0a241b5c9b7a77ed572b3f19a47d9310c9aea2a
David Asabina <vid@bina.me>
2025-01-19 01:55:04 +0100

    feat: stub side panel
    based on https://wxt.dev/guide/essentials/entrypoints.html#side-panel

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -5,6 +5,16 @@ import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const openSidePanel = () => {
+		// TODO: Remove the popup entrypoint
+		// It seems like it may be an either/or deal since I added sidepanel.
+		// Couldn't get both to work, so this code was running before, but
+		// didn't pull up the sidepanel and a fresh respawn of the browser, now
+		// has the sidepanel rendering.
+		browser.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+		console.log("opening?!?");
+		console.log(browser);
+	};
 
 	return (
 		<>
@@ -19,6 +29,7 @@ function App() {
 			<h1>WXT + React</h1>
 			<div className="card">
 				<button onClick={() => { setCount((count) => count + 1); }}>count is {count}</button>
+				<button onClick={() => { openSidePanel() }}>open side panel</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
